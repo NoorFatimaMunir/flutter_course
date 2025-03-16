ba1c10ef57454635ca63c43d267af99edb98b0a7
Noor Fatima <noorfatimagrw965@gmail.com>
2025-03-16 11:29:18 -0700

    Initial commit

---
+++ lib/function.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+void Add(int x, int y){
+  x+y;
+
+}
+void demo(){
+  Add(2,3);
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(
+    MaterialApp(home: Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(child: Text("Hello world")))),
+  );
+}
